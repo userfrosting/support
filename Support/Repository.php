62c7bd4a3c17514d3b18a49650229b579b71ac99
alexweissman <alex@alexanderweissman.com>
2017-06-10 02:00:47 -0400

    begin implementing basic Repository

---
+++ Support/Repository.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/fortress
+ * @license   https://github.com/userfrosting/fortress/blob/master/licenses/UserFrosting.md (MIT License)
+ */
+namespace UserFrosting\Support;
+
+use Illuminate\Config\Repository as IlluminateRepository;
+
+class Repository extends IlluminateRepository
+{
+    /**
+     * Recursively merge locale values (scalar or array) into this repository.
+     *
+     * If no key is specified, the items will be merged in starting from the top level of the array.
+     * If a key IS specified, items will be merged into that key.
+     * Nested keys may be specified using dot syntax.
+     * @param string|null $key
+     * @param mixed $items
+     */
+    public function mergeItems($key = null, $items)
+    {
+        $targetValues = array_get($this->items, $key);
+
+        if (is_array($targetValues)) {
+            $modifiedValues = array_replace_recursive($targetValues, $items);
+        } else {
+            $modifiedValues = $items;
+        }
+
+        array_set($this->items, $key, $modifiedValues);
+    }
+}
